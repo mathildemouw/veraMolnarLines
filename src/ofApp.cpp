2daea2412ea1cdeeff2b197837a5f5126947580e
Mathilde <nihaomouw@mac.com>
2019-10-13 14:22:52 -0400

    Always seed with mouseX so I can experiment with different random placements

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -13,15 +13,25 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    // horizontal lines
+    // first layer: horizontal lines
     float i;
     float newi;
     float offsetFromBottom  = 400;
     float marginLeft = 50;
     float marginRight = ofGetScreenWidth() - marginLeft;
 
-    // TODO: explore varying this line to get closer to Molnár's variances
-    ofSeedRandom(67);
+    // playing with noise and randomness
+    // considerations:
+
+    // max min lengths of Vera Molnár's angled lines
+    // also frequency of them
+    // also overlappingness, cluster them together more by using Noise instead of Random
+
+    // min max length of interruptions
+    // frequency of interruptions
+
+    ofSeedRandom(mouseX);
+    // 67
     ofSetColor(0);
     
     for (i =  0; i < 20; i ++) {
@@ -31,14 +41,14 @@ void ofApp::draw(){
                    (ofNoise(i * 0.9) * 200.0) + marginLeft,
                    newi + 200,
                    // draw lines all to be about the same length
-                   // TODO: explore varying this line to get closer to Molnár's variances
                    ((ofNoise(i * 0.9) * 200.0) + marginLeft) + (ofGetScreenWidth() / 2) + ofRandom(20),
                    newi + 200
                    );
     }
 
-    // deletion of horizontal line portions
-    ofSeedRandom(400 * 20);
+    // second layer: deletion of horizontal line portions
+    ofSeedRandom(mouseX);
+    // 400 * 20
     ofSetColor(255);
     
     for (i =  0; i < 20; i ++) {
@@ -46,28 +56,24 @@ void ofApp::draw(){
       ofDrawRectangle(ofRandom(ofGetScreenWidth() * 2), newi + 197 , ofRandom(20), 20);
     }
 
-    // short angled lines
-    
-    // length of line is mostly controlled by slopeX
-    // angle of line is mostly controlled by slopeY
-
-    // [x] each horizontal line has one, two three, four, five or 0 short lines lying on it from their centers
-    // [] short angled lines always bisect horizontal lines
-    
+    // last layer: short angled lines
     ofSetColor(0);
-    // TODO: play around with  this seed to adjust slopes and frequency of small lines crossing horizontal lines
     ofSeedRandom(mouseX);
     cout  <<  mouseX << endl;
     // 326 is a  pretty good one
     
      for (i =  0; i < 20; i ++) {
          newi = ofMap(i, 0, 20, 0, ofGetScreenHeight() - offsetFromBottom);
-         float j;
+
+         // centerX controls where on the horizontal it lands
          float centerX = ofRandom(700) + 200;//(ofNoise(i * 0.9) * 2000.0);
          float centerY = newi + 200;
+         // angle of line is mostly controlled by slopeY
          float slopeY = ofRandom(25) + 5;
+         // length of line is mostly controlled by slopeX
          float slopeX = ofRandom(40) + 5;
 
+         float j;
          for (j =  0; j < ofRandom(5); j ++) {
              ofDrawLine(centerX + slopeX,
                         centerY - slopeY,
